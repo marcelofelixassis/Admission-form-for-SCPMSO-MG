e84f6a1d52dc08470a49655b7e913de0cac6fb41
Marcelo Félix <marcelofelix.af@gmail.com>
2018-02-02 13:41:51 -0200

    ultimas alterações

--- index.php
+++ index.php
@@ -14,38 +14,48 @@
 	</head>
 
 	<body>
-    <div class="toolbar">
-      <h3>PROTOCOLO DE AVALIAÇÃO FONOAUDIOLOGICA PERIODICA DA VOZ DO PROFESSOR</h3>
-      <h4>SCMPSO/SEPLAG</h4>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand title" href="http://10.16.90.76">PROTOCOLO DE AVALIAÇÃO FONOAUDIOLÓGICA PERIÓDICA DA VOZ DO PROFESSOR</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a href="http://10.16.90.76/formulario">
+        <button type="button" class="btn btn-primary" >
+          <span class="glyphicon glyphicon-plus"></span> Novo Formulário
+        </button>
+      </a>
+      </ul>
+        <div class="col-md-8" style="float: right;">
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-btn">
+                <select class="btn" id="maspcpf">
+                    <option value="1">CPF</option>
+                    <option value="2">MASP</option>
+                </select>
+              </span>
+
+              <input type="text" class="form-control" id="inp_buscar" maxlength="14" onkeypress="mascara(this,cpf)">
+                  
+              <span class="input-group-btn">
+                <button class="btn btn-danger" type="button" onclick="buscar()">Buscar</button>
+              </span>
+            </div>
+          </div>
+        </div>
     </div>
+  </nav>
+
+   
     <div class="container">	
-      <a href="http://10.16.90.76/formulario">
-			  <button type="button" class="btn btn-primary" style="margin-top: 10px;">
-			    <span class="glyphicon glyphicon-plus"></span> Novo Formulário
-			  </button>
-			</a>
+      
 		  
-  		<div class="col-md-4" style="float: right; margin-top: 10px;">
-  			<div class="form-group">
-    			<div class="input-group">
-						<span class="input-group-btn">
-    					<select class="btn" id="maspcpf">
-      						<option value="1">CPF</option>
-      						<option value="2">MASP</option>
-    					</select>
-      			</span>
 
-      			<input type="text" class="form-control" id="inp_buscar">
-      					
-        		<span class="input-group-btn">
-        			<button class="btn btn-primary" type="button" onclick="buscar()">Buscar</button>
-      			</span>
-    			</div>
-  			</div>
-  		</div>
-
-
-      <table class="table table-dark table-hover" style="margin-top: 10px;" id="tabela_ultimos_registros">
+      <table class="table table-active table-hover" style="margin-top: 10px;" id="tabela_ultimos_registros">
     		<thead>
       		<tr>
     				<th>Nome</th>
--- js/scripts_painel/painel.js
+++ js/scripts_painel/painel.js
@@ -54,12 +54,12 @@ function inserir_resultado(data){
                         '<div class="btn-group btn-group-sm">' + 
                             '<div class="btn-group"> ' +
                                 '<a href="#" class="parents js-view-parents">' +
-                                    '<span class="glyphicon glyphicon-eye-open" style="color:white;" onclick="visualizar('+ data[index].id_finalidade +','+ data[index].id_pessoa +')"></span>' +
+                                    '<span class="glyphicon glyphicon-eye-open" style="color:black;" onclick="visualizar('+ data[index].id_finalidade +','+ data[index].id_pessoa +')"></span>' +
                                 '</a>' +
                             '</div>' +
                             '<div class="btn-group ">' +
                                 '<a href="#">' +
-                                '<span class="glyphicon glyphicon-pencil" aria-hidden="true" style="color: white; margin-left: 10px;" onclick="editar('+ data[index].id_finalidade +','+ data[index].id_pessoa +')"></span>' +
+                                '<span class="glyphicon glyphicon-pencil" aria-hidden="true" style="color: black; margin-left: 10px;" onclick="editar('+ data[index].id_finalidade +','+ data[index].id_pessoa +')"></span>' +
                                 '</a>' +
                             '</div>' +
                         '</div>' +
@@ -79,7 +79,7 @@ function inserir_resultado(data){
                         '<div class="btn-group btn-group-sm">' + 
                             '<div class="btn-group"> ' +
                                 '<a href="#" class="parents js-view-parents">' +
-                                    '<span class="glyphicon glyphicon-eye-open" style="color:white;" onclick="visualizar('+ data[index].id_finalidade +','+ data[index].id_pessoa +')"></span>' +
+                                    '<span class="glyphicon glyphicon-eye-open" style="color:black;" onclick="visualizar('+ data[index].id_finalidade +','+ data[index].id_pessoa +')"></span>' +
                                 '</a>' +
                             '</div>' +
                         '</div>' +
@@ -99,5 +99,31 @@ function visualizar(finalidade, identificacao) {
 
 function editar(finalidade, identificacao) {
     window.open("http://10.16.90.76/editar_formulario/index.php?id="+identificacao+"&fi="+finalidade);
+}
+
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)    
+}
+
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+
+function cpf(v){
+    if(document.getElementById("maspcpf").value == 1){
+        v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito
+        v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
+        v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos de novo (para o segundo bloco de números)
+        v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2") //Coloca um hífen entre o terceiro e o quarto dígitos
+        return v    
+    }else{
+        v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito
+        v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
+        v=v.replace(/(\d{2})(\d{1,1})$/,"$1-$2") //Coloca um hífen entre o terceiro e o quarto dígitos
+        return v
+
+    }
 }
 
